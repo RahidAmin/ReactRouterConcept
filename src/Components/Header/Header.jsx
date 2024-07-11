9e47d0a03afcbda260d5b1f8fded869ad638f013
RahidAmin <rahidamin19@gmail.com>
2024-07-11 22:45:03 +0600

    use navigation and useLocation added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
@@ -7,11 +7,16 @@ const Header = () => {
 
             <nav>
                 <span>My WebSite</span>
-                <Link to="/">Home</Link>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/users'>Users</NavLink>
+                <NavLink to='/posts'>Posts</NavLink>
+                <NavLink to='/about'>About</NavLink>
+                <NavLink to='/contact'>Contact</NavLink>
+                {/* <Link to="/">Home</Link>
                 <Link to="/users">Users</Link>
                 <Link to="/posts">Posts</Link>
                 <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
+                <Link to="/contact">Contact</Link> */}
                 {/* <a href="/">Home</a><a href="/about">About</a><a href="/contact">Contact Us</a> */}
             </nav>
         </div>
